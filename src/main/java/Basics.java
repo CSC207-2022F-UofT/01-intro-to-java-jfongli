300473a461393b159ea0a30905567c8d1f99a0a9
jfong <j.fongli@mail.utoronto.ca>
2022-09-25 20:09:39 -0400

    Third attempt :,)

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -103,8 +103,8 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("Current count:" + i);
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
         }
         // Why does this piece of code not run when I write i == 10 as the termination?
 
@@ -151,7 +151,7 @@ public class Basics {
 
         // Fill in the rest of the body here
         String[] separate_words = to_split.split(" ");
-        for (int i = 0; i <= 7; i++) {
+        for (int i = 0; i < 7; i++) {
             ret.append(separate_words[i].charAt(0));
         }
         return ret.toString();
@@ -182,7 +182,7 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-        for (int i = 0; i <= arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if ( i % 2 != 0) {
                 current_sum += arr[i];
             }
